98cf114efc4465fa83d84f63234cf83e25cb3de4
Tkachenko Natalia <nataprofits@gmail.com>
2023-08-02 16:50:27 +0100

    update functions

--- contacts.js
+++ contacts.js
@@ -1,19 +1,47 @@
 const fs = require('fs/promises');
-const contactsPath = require("./db/contacts.json");
+const { nanoid } = require('nanoid');
+const path = require("path");
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код. Возвращает массив контактов.
+const contactsPath = path.join(__dirname,"db", "contacts.json");
+console.log(contactsPath);
+
+
+const listContacts = async () => {
+const data = await fs.readFile(contactsPath);
+return JSON.parse(data);
 }
 
-function getContactById(contactId) {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+const getContactById = async (contactId) => {
+ const contacts = await listContacts();
+ const targetContact = contacts.find(item => item.id === contactId);
+ return targetContact || null;
 }
 
-function removeContact(contactId) {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-}
+ const removeContact = async(contactId) => {
+const contacts = await listContacts();
+const index = contacts.findIndex(item => item.id === contactId);
+if(index === -1) {
+  return null;
+} 
+const [result] = contacts.splice(index, 1);
+await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+return result;
+ }
 
-function addContact(name, email, phone) {
-  // ...твой код. Возвращает объект добавленного контакта. 
+ const addContact = async(data) => {
+const contacts = await listContacts();
+const newContact = {
+  id: nanoid(),
+  ...data,
+};
+contacts.push(newContact);
+await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+return newContact;
+ }
+
+module.exports = {
+listContacts,
+getContactById,
+addContact,
+removeContact,
 }
--- index.js
+++ index.js
@@ -1,65 +1,40 @@
-const readline = require('readline');
-const fs = require('fs').promises;
-const { program } = require('commander');
-require('colors');
-program.option(
-  '-f, --file [type]',
-  'file for saving game results',
-  'results.txt',
-);
+const { Command } = require("commander");
+const program = new Command;
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
 program.parse(process.argv);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const argv = program.opts();
 
-let count = 0;
-const logFile = program.opts().file;
-const mind = Math.floor(Math.random() * 10) + 1;
+const contacts = require("./contacts");
 
-const isValid = value => {
-  if (isNaN(value)) {
-    console.log('Введите число!'.red);
-    return false;
-  }
-  if (value < 1 || value > 10) {
-    console.log('Число должно быть в диапазоне 1 до 10'.red);
-    return false;
-  }
-  return true;
-};
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
 
-const log = async data => {
-  try {
-    await fs.appendFile(logFile, `${data}\n`);
-    console.log(`Удалось сохранить результат в файл ${logFile}`.green);
-  } catch (err) {
-    console.log(`Не удалось сохранить файл ${logFile}`.red);
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case 'remove':
+     const removeContact = await contacts.removeContact(id);
+     return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-const game = () => {
-  rl.question(
-    'Введите число от 1 до 10, чтобы угадать задуманное: '.yellow,
-    value => {
-      let a = +value;
-      if (!isValid(a)) {
-        game();
-        return;
-      }
-      count += 1;
-      if (a === mind) {
-        console.log('Поздравляю Вы угадали число за %d шага(ов)'.green, count);
-        log(
-          `${new Date().toLocaleDateString()}: Поздравляю Вы угадали число за ${count} шага(ов)`,
-        ).finally(() => rl.close());
-        return;
-      }
-      console.log('Вы не угадали еще попытка'.red);
-      game();
-    },
-  );
-};
-
-game();
+invokeAction(argv);
